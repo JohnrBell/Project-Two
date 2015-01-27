264dc339145646dbfdd39df4ce511af4584bd43c
JohnrBell <johnrbell@gmail.com>
2015-01-27 18:07:16 -0500

    gonna break my db

--- db/connection.rb
+++ db/connection.rb
@@ -1,10 +1,14 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection({
-  :adapter => "postgresql",
-  :database => "contact_list",
-  :host => "localhost",
-  :username => "john"
-})
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/d16e7ub767ulkn')
 
+# require 'active_record'
+
+# ActiveRecord::Base.establish_connection({
+#   :adapter => "postgresql",
+#   :database => "contact_list",
+#   :host => "localhost",
+#   :username => "john"
+# })
+# 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
